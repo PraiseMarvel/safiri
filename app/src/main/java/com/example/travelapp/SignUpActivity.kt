dcdb752561e8e7d5b74bb6717302940600d52717
PraiseMarvel <omollopraise@gmail.com>
2023-07-06 18:52:03 +0300

    project app travel

---
+++ app/src/main/java/com/example/travelapp/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.example.travelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+
+ class SignUpActivity : AppCompatActivity() {
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/travelapp/login.kt
@@ -0,0 +1,14 @@
+package com.example.travelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
